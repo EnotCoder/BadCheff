5bc85aaed73b40fdc1055df151281d81c8c78046
EnoticCoder <samaks.sasha@gmail.com>
2026-09-05 20:33:32 +0300

    update audio

--- enemy/State.gd
+++ enemy/State.gd
@@ -26,19 +26,12 @@ func _ready():
 	noise_sound.bus = "Master"
 	add_child(noise_sound)
 
-	player_sound = AudioStreamPlayer.new()
-	player_sound.stream = load("res://sounds/player/ЧЁРТ.mp3")
-	player_sound.bus = "Master"
-	add_child(player_sound)
-
 func set_noise():
 	if audio_busy or timer_active:
 		noise.emit()
 		return
 	audio_busy = true
 	await get_tree().create_timer(0.5).timeout
-	player_sound.play()
-	await player_sound.finished
 	noise_sound.play()
 	await noise_sound.finished
 	audio_busy = false
