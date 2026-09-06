2b0b2eecb4a1796af005d0af5e15d6297ec2967c
EnoticCoder <samaks.sasha@gmail.com>
2026-09-06 10:22:13 +0300

    fix: disable auto-chase during chef investigation

--- enemy/cheff_ai.gd
+++ enemy/cheff_ai.gd
@@ -54,7 +54,7 @@ func process_movement() -> void:
 	if State.position_point == "kitchen" and (
 		door_kitchen.get("door_opened") as bool
 		or MainInventoryScript.position_point != "frezz_room"
-	) and State.state != State.StateBook.ATTACK:
+	) and State.state == State.StateBook.IDLE:
 		_start_chase()
 
 	if !navigation_agent.is_navigation_finished():
