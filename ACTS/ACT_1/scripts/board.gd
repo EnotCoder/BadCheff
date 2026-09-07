f016e0a253c993e934a5fa1f0b1d37f0c2ceac7f
EnoticCoder <samaks.sasha@gmail.com>
2026-09-07 16:55:51 +0300

    update board

--- ACTS/ACT_1/scripts/board.gd
+++ ACTS/ACT_1/scripts/board.gd
@@ -1,8 +1,14 @@
 extends Node3D
 
 @onready var action: Area3D = $action
+@onready var def_mesh: Node3D = $def_mesh
+@onready var break_mesh: Node3D = $break_mesh
 
 var is_break:bool = true
+
+func _ready() -> void:
+	break_mesh.position = Vector3.ZERO
+	break_mesh.hide()
 
 func break_board() -> void:
 	var is_hamer = (MainInventoryScript.chose_obj == "hamer")
@@ -14,8 +20,8 @@ func break_board() -> void:
 		Dialog.show_hint("Мне нужен молоток")
 
 func replace_model_board() -> void:
-	$def_mesh.hide()
-	$break_mesh.show()
+	def_mesh.hide()
+	break_mesh.show()
 	
 func delete_action() -> void:
 	if action != null:
