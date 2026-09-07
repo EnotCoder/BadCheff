0b373b6f0bb6b13053bbf5804e4a3f0d91115691
EnoticCoder <samaks.sasha@gmail.com>
2026-09-07 17:10:20 +0300

    update menu

--- ACTS/scripts/chapter_chose.gd
+++ ACTS/scripts/chapter_chose.gd
@@ -7,4 +7,4 @@ func _ready() -> void:
 		if child is TextureButton:
 			UIAnimations.setup_texture_button_hover(child)
 	$chapter_one.pressed.connect(func() -> void: chapter_selected.emit("res://ACTS/ACT_1/scenes/act_1_prolog.tscn"))
-	$chapter_two.pressed.connect(func() -> void: chapter_selected.emit("res://ACTS/ACT_2/scenes/act_2.tscn"))
+	#$chapter_two.pressed.connect(func() -> void: chapter_selected.emit("res://ACTS/ACT_2/scenes/act_2.tscn"))
