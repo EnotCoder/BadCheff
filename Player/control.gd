9de9bb6f52c48e6db5ea5e8a34794166b3d4558e
EnoticCoder <samaks.sasha@gmail.com>
2026-09-08 14:22:25 +0300

    fix bag

--- Player/control.gd
+++ Player/control.gd
@@ -1,32 +1,39 @@
 extends Node2D
 
+func _press_and_release(action: StringName) -> void:
+	var a = InputEventAction.new()
+	a.action = action
+	a.pressed = true
+	Input.parse_input_event(a)
+	
+	# Give it a frame to be processed by consumers like is_action_just_pressed
+	await get_tree().process_frame
+	
+	var b = InputEventAction.new()
+	b.action = action
+	b.pressed = false
+	Input.parse_input_event(b)
 
 
-func _on_sit_pressed() -> void :
-	Input.action_press("sit")
-	Input.action_release("sit")
+func _on_sit_pressed() -> void:
+	_press_and_release("sit")
 
 
-func _on_interact_pressed() -> void :
-	Input.action_press("left_click")
-	Input.action_release("left_click")
+func _on_interact_pressed() -> void:
+	_press_and_release("left_click")
 
 
-func _on_slot_pressed() -> void :
-	Input.action_press("slot_1")
-	Input.action_release("slot_1")
+func _on_slot_pressed() -> void:
+	_press_and_release("slot_1")
 
 
-func _on_slot_2_pressed() -> void :
-	Input.action_press("slot_2")
-	Input.action_release("slot_2")
+func _on_slot_2_pressed() -> void:
+	_press_and_release("slot_2")
 
 
-func _on_slot_3_pressed() -> void :
-	Input.action_press("slot_3")
-	Input.action_release("slot_3")
+func _on_slot_3_pressed() -> void:
+	_press_and_release("slot_3")
 
 
-func _on_drop_pressed() -> void :
-	Input.action_press("drop")
-	Input.action_release("drop")
+func _on_drop_pressed() -> void:
+	_press_and_release("drop")
